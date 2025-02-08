470331961e3ea367c20f109df64980689b31e4bd
GiolenaK <g.kokorou@acg.edu>
2025-02-08 19:06:54 +0200

    profile layout works, if statements with placeholders

--- users/views.py
+++ users/views.py
@@ -6,5 +6,7 @@ def index(request):
     return render(request, 'users/index.html')
 
 def profile(request):
-    print("hellooooooo")
-    return render(request, 'users/profile.html')
+    random_user = {'username': 'Giolena'}
+    random_user2 = {'username': 'Kat'}
+    context = random_user
+    return render(request, 'users/profile.html',{'user':context})
