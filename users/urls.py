210adcd88e7d3250bf55ce86c7ef23b18b9b8d2b
GiolenaK <g.kokorou@acg.edu>
2025-02-08 20:55:40 +0200

    working badge page, with placeholder text

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='user-index'),
     path('profile/', views.profile, name='user-profile'),
+    path('profile/badge', views.badge, name='user-badge'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,7 +6,19 @@ def index(request):
     return render(request, 'users/index.html')
 
 def profile(request):
-    random_user = {'username': 'Giolena'}
-    random_user2 = {'username': 'Kat'}
-    context = random_user
+    usertype1 = {'status': 'Fosterer'}
+    usertype2 = {'status': 'Admin'}
+    usertype3 = {'status': 'Staff'}
+    context = usertype1
     return render(request, 'users/profile.html',{'user':context})
+
+def badge(request):
+    badgestatus1 = {'status': 'Accepted'}
+    badgestatus2 = {'status': 'Pending'}
+    badgestatus3 = {'status': 'Rejected'}
+    badgestatus4 = {'status': 'Admin'}
+    badgestatus5 = {'status': 'Staff'}
+    badgestatus6 = {'status': 'None'}
+    context = badgestatus1
+    return render(request, 'users/badge.html',{'status':context})
+   
