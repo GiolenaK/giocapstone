f5c4e8f5fb250e664c2a0593b9b6de2a47d92b10
GiolenaK <g.kokorou@acg.edu>
2025-02-09 17:23:17 +0200

    sign up and log in design done

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='user-index'),
     path('login/', views.login, name='user-login'),
+    path('signup/', views.signup, name='user-signup'),
     path('profile/', views.profile, name='user-profile'),
     path('profile/badge', views.badge, name='user-badge'),
-    path('profile/editprofile', views.editprofile, name='user-edit-profile'),
+    path('profile/edit-profile', views.editprofile, name='user-edit-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -27,3 +27,7 @@ def editprofile(request):
 
 def login(request):
    return render(request, 'users/login.html')
+
+
+def signup(request):
+   return render(request, 'users/signup.html')
